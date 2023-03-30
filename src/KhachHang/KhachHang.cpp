5dc7c7ead8a917d0f54f1848a3dce609edae2a46
anhvu <anhvu2332003@gmail.com>
2023-03-30 23:27:59 +0700

    Tao class KhachHang

---
+++ src/KhachHang/KhachHang.cpp
@@ -0,0 +1,5 @@
+//
+// Created by anhvu on 3/30/2023.
+//
+
+#include "KhachHang.h"
---
+++ src/KhachHang/KhachHang.h
@@ -0,0 +1,15 @@
+//
+// Created by anhvu on 3/30/2023.
+//
+
+#ifndef CPP_QLKS_KHACHHANG_H
+#define CPP_QLKS_KHACHHANG_H
+
+
+class KhachHang {
+private:
+    string ten;
+};
+
+
+#endif //CPP_QLKS_KHACHHANG_H
