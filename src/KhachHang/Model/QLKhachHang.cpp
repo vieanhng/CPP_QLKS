3eebdf025b95481b84a64c1d25986df7ce17705b
vananh <vananh@gmail.com>
2023-04-04 15:58:36 +0700

    update qlkhachhang

--- src/KhachHang/Model/QLKhachHang.cpp
+++ src/KhachHang/Model/QLKhachHang.cpp
@@ -17,11 +17,46 @@ void QLKhachHang::suaKhachHang() {
 }
 
 void QLKhachHang::dsKhachHang() {
+    try {
+        tabulate::Table khachhangTable;
+        cout << "DANH SACH KHACH HANG"<<endl;
+        khachhangTable.add_row({"ma khach hang","ten khach hang","dia chi","so dien thoai","email"});
+        ss.str("");
+        ss << "select * from khach hang";
+        std::string query = ss.str();
+        MYSQL_RES* res = exec_query(query.c_str());
+        MYSQL_ROW row;
+        while ((row = mysql_fetch_row(res))) {
+            khachhangTable.add_row({row[0], row[1],row[2],row[3],row[4]});
+        }
+        std::cout << khachhangTable << std::endl;
+        mysql_free_result(res);
+    }catch (std::exception& e) {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
 
 }
 
 KhachHang *QLKhachHang::loadKhachHang(int maKh) {
-    return nullptr;
+    KhachHang* khachhang = NULL;
+    ss.str("");
+    ss << "SELECT * FROM khachhang WHERE ma_kh=" << maKh;
+    std::string query = ss.str();
+
+    MYSQL_RES* res = exec_query(query.c_str());
+    MYSQL_ROW row = mysql_fetch_row(res);
+
+    if (row != NULL) {
+        khachhang = new KhachHang();
+        khachhang->setMaKh(maKh);
+        khachhang->setTenKh(row[1]);
+        khachhang->setDiaChi(row[2]);
+        khachhang->setSdt(row[3]);
+        khachhang->setEmail(row[4]);
+    }
+
+    mysql_free_result(res);
+    return khachhang;
 }
 
 bool QLKhachHang::existKhachHang(int ma_kh) {
--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,11 @@
 #include "Database/Database.h"
 #include "Phong/Phong.h"
 #include "Phong/Model/QLPhong.h"
+#include "KhachHang/Model/QLKhachHang.h"
 
 using namespace std;
 int main(int argc, char** argv) {
-    QLPhong ql;
-    ql.taoPhong();
+    QLKhachHang kh;
+    kh.dsKhachHang();
     return 0;
 }
