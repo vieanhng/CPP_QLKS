37ed6db4b309bba429b3f464091f6ad7c31a2dd2
Quynh Anh <quanh06102003@gmail.com>
2023-04-11 23:28:59 +0700

    Tao file HoaDon

--- src/HoaDon/HoaDon.cpp
+++ src/HoaDon/HoaDon.cpp
@@ -4,16 +4,67 @@
 
 #include "HoaDon.h"
 
-HoaDon::HoaDon() {}
+HoaDon::HoaDon(int soHD, std::string ngaylap, int soTien, int tinhtrangThanhToan, int madatPhong):
+        so_HD(soHD),ngay_lap(ngaylap),so_tien(soTien),tinh_trang_thanh_toan(tinhtrangThanhToan),ma_dat_phong(madatPhong) {}
 
-HoaDon::~HoaDon() {
-
+void HoaDon::nhap() {
+    {
+        this->ngay_lap = getString("Nhap ngay lap:", 50);
+        this->so_tien = getNumber("Nhap so tien: ");
+        this->tinh_trang_thanh_toan = getNumber("Nhap tinh trang thanh toan(1:Da thanh toan, 0:Chua thanh toan): ");
+        this->ma_dat_phong = getNumber("Nhap ma dat phong: ");
+    }
 }
 
-HoaDon::DatPhong(int soHD, const std::string &ngaylap,
-                   const std::string &tinhTrangThanhToan) : so_hd(soHD),
-                                                            ngay_lap(ngaylap),
-                                                            tinh_trang_thanh_toan(
-                                                                    tinhTrangThanhToan),  {}
+int HoaDon::getsoHD() const {
+    return so_HD;
+}
+
+void HoaDon::setsoHD(int soHD) {
+    so_HD = soHD;
+}
+
+const std::string &HoaDon::getngaylap() const {
+    return ngay_lap;
+}
+
+void HoaDon::setngaylap(const std::string &ngaylap) {
+    this->ngay_lap = ngaylap;
+}
+
+int HoaDon::getsoTien() const {
+    return so_tien;
+}
+
+void HoaDon::setsoTien(int soTien) {
+    this->so_tien = soTien;
+}
+
+int HoaDon::gettinhtrangThanhToan() const {
+    return tinh_trang_thanh_toan;
+}
+
+void HoaDon::settinhtrangThanhToan(int tinhtrangThanhToan) {
+    this->tinh_trang_thanh_toan = tinhtrangThanhToan;
+}
+
+int HoaDon::getmadatPhong() const {
+    return ma_dat_phong;
+}
+
+void HoaDon::setmadatPhong(int madatPhong) {
+    this->ma_dat_phong = madatPhong;
+}
+
+void HoaDon::hienThiThongTin() {
+    std::cout << "Thong tin phong:";
+    std::cout << "\n====================================\n";
+    std::cout << "Ma phong: " << getsoHD();
+    std::cout << "\nLoai phong: " << getngaylap();
+    std::cout << "\nSo phong: " << getsoTien();
+    std::cout << "\nGia phong: " << gettinhtrangThanhToan();
+    std::cout << "\nTinh trang phong: " << getmadatPhong();
+    std::cout << "\n====================================\n";
+}
 
 
--- src/HoaDon/HoaDon.h
+++ src/HoaDon/HoaDon.h
@@ -5,10 +5,50 @@
 #ifndef CPP_QLKS_HOADON_H
 #define CPP_QLKS_HOADON_H
 
+#include <iostream>
+#include "../Utils/Utils.h"
 
 class HoaDon {
+private:
+    int so_HD;
+    std::string ngay_lap;
+    int so_tien;
+    int tinh_trang_thanh_toan;
+    int ma_dat_phong;
+public:
+    HoaDon(int soHD, std::string ngaylap,int soTien,int tinhtrangThanhToan,int madatPhong);
+    HoaDon(){};
 
+    void nhap();
+
+
+    void hienThiThongTin();
+
+    int getsoHD() const;
+
+    void setsoHD(int soHD);
+
+    const std::string &getngaylap() const;
+
+    void setngaylap(const std::string &ngaylap);
+
+    int getsoTien() const;
+
+    void setsoTien(int soTien);
+
+    int gettinhtrangThanhToan() const;
+
+    void settinhtrangThanhToan(int tinhtrangThanhToan);
+
+    int getmadatPhong() const;
+
+    void setmadatPhong(int madatPhong);
 };
 
 
 #endif //CPP_QLKS_HOADON_H
+
+
+
+
+
