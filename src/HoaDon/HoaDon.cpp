47a0be7f5523daffd81d4dd3bea892a82be86c3d
Quynh Anh <quanh06102003@gmail.com>
2023-04-11 22:20:34 +0700

    Tao file HoaDon

---
+++ src/HoaDon/HoaDon.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Quynh Anh on 11/4/2023.
+//
+
+#include "HoaDon.h"
+
+HoaDon::HoaDon() {}
+
+HoaDon::~HoaDon() {
+
+}
+
+HoaDon::DatPhong(int soHD, const std::string &ngaylap,
+                   const std::string &tinhTrangThanhToan) : so_hd(soHD),
+                                                            ngay_lap(ngaylap),
+                                                            tinh_trang_thanh_toan(
+                                                                    tinhTrangThanhToan),  {}
+
+
---
+++ src/HoaDon/HoaDon.h
@@ -0,0 +1,14 @@
+//
+// Created by test1 on 11/4/2023.
+//
+
+#ifndef CPP_QLKS_HOADON_H
+#define CPP_QLKS_HOADON_H
+
+
+class HoaDon {
+
+};
+
+
+#endif //CPP_QLKS_HOADON_H
