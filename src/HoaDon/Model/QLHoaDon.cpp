58f6a6a58e685a7c9c9c7c0da98fd7bb4aa89400
Quynh Anh <quanh06102003@gmail.com>
2023-04-15 23:10:49 +0700

    Cap nhat dsHoaDon trong QLHoaDon.cpp

--- src/HoaDon/Model/QLHoaDon.cpp
+++ src/HoaDon/Model/QLHoaDon.cpp
@@ -18,7 +18,25 @@ void QLHoaDon::suaHoaDon() {
 
 
 void QLHoaDon::dsHoaDon() {
-
+    try {
+        tabulate::Table hoadonTable;
+        cout << "DANH SACH HOA DON"<<endl;
+        hoadonTable.add_row({"so_hd","ngay_lap","so_tien","tinh_trang_thanh_toan","ma_dat_phong"});
+        ss.str("");
+        ss << "select * from hoa_don";
+        std::string query = ss.str();
+        MYSQL_RES* res = db.exec_query(query.c_str());
+        MYSQL_ROW row;
+        int i = 1;
+        while ((row = mysql_fetch_row(res))) {
+            hoadonTable.add_row({std::to_string(i),row[0], row[1],row[2],row[3],row[4],row[5]});
+            i++;
+        }
+        std::cout << hoadonTable << std::endl;
+        mysql_free_result(res);
+    }catch (std::exception& e) {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
 }
 
 HoaDon *QLHoaDon::loadHoaDon(int soHD) {
